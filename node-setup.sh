d8fb4b1e7b5421f9a18625edcf7d315543a59328
Nico Pretorius <nico.pretorius@gmx.com>
2014-06-27 21:47:45 +0200

    Update node-setup.sh

--- node-setup.sh
+++ node-setup.sh
@@ -17,6 +17,9 @@ sudo yum install gcc-c++ make
 sudo yum install openssl-devel
 sudo yum install git
 git clone git://github.com/joyent/node.git
+
+--> continue here!!!
+
 cd node
 git checkout
 ./configure
